43213e2d8d8ce6721155f491b10c94e19449f5e2
AlexN <alexandranayanzina@gmail.com>
2025-01-04 23:07:39 -0500

    Example of extracting a single value from thr response using extract().response().path()

---
+++ src/test/java/com/rest/ExtractSingleValueFromResponseUsingResponsePathTest.java
@@ -0,0 +1,33 @@
+package com.rest;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class ExtractSingleValueFromResponseUsingResponsePathTest {
+
+    @Test
+    public void extractSingleValueFromResponseUsingPathTest() {
+
+        Keys key = Keys.X_API_KEY;
+
+        String name = given()
+                .baseUri("https://api.postman.com")
+                .header("X-api-key", key.getKey())
+                .header("Accept-Encoding", "gzip, deflate, br\n").
+                when()
+                .get("/workspaces").
+                then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .response()
+                .path("workspaces[0].name");
+
+        System.out.println("First workspace name: " + name);
+
+    }
+}
