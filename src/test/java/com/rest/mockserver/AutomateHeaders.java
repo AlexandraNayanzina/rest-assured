9bac514eb9673671c45a1b9a8a74818003c277d1
AlexN <alexandranayanzina@gmail.com>
2025-01-05 22:50:02 -0500

    Multiple Headers using HashMap collections of headers

--- src/test/java/com/rest/mockserver/AutomateHeaders.java
+++ src/test/java/com/rest/mockserver/AutomateHeaders.java
@@ -3,22 +3,31 @@ package com.rest.mockserver;
 import io.restassured.http.Header;
 import io.restassured.http.Headers;
 import org.testng.annotations.Test;
+
+import java.util.HashMap;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
+import java.util.HashMap;
 
 public class AutomateHeaders {
-
-    Header header = new Header("headerName2", "value2");
-    Header matchHeader = new Header("x-mock-match-request-headers", "headerName2");
-
-    Headers headers = new Headers(header, matchHeader);
 
     @Test
     public void multipleHeadersTest() {
 
+        Header header = new Header("headerName2", "value2");
+        Header matchHeader = new Header("x-mock-match-request-headers", "headerName2");
+
+        Headers headers = new Headers(header, matchHeader);
+
+        HashMap<String, String> headersMap = new HashMap<>();
+        headersMap.put("headerName2", "value2");
+        headersMap.put("x-mock-match-request-headers", "headerName2");
+
+
         given()
                 .baseUri("https://1623b81a-ab10-403e-a861-f123939bc23b.mock.pstmn.io")
-                .headers(headers)
+                .headers(headersMap)
                 .log().all().
                 when()
                 .get("/get").
